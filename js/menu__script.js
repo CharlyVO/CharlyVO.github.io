0d37acd4e2c8478be346159976df5c8eae80ee2e
CharlyVO <80501459+CharlyVO@users.noreply.github.com>
2021-12-05 18:10:33 -0600

    Add files via upload

--- js/menu__script.js
+++ js/menu__script.js
@@ -4,15 +4,6 @@ const navItems = document.querySelectorAll('#nav__item');
 const btnOpenMenu = document.querySelector('#btn__open-menu');
 const btnCloseMenu = document.querySelector('#btn__close-menu');
 
-/* Funcion para dejar efecto hover en el link actual */
-function activedLink() {
-    navItems.forEach((item) => item.classList.remove('hovered'));
-    this.classList.add('hovered');
-}
-
-navItems.forEach((item) => item.addEventListener('mouseover', activedLink));
-
-
 /* Funcion para cambiar el color de la barra de navegacion */
 window.onscroll = () => {
     if(!navBar.classList.contains('nav__dark-mode')) {
@@ -37,3 +28,23 @@ navItems.forEach((item) => item.addEventListener('click', () => {
     navMenu.classList.remove('show__menu');
     btnOpenMenu.classList.remove('hide__btn');
 }));
+
+/* Cambiamos el Indicador del Menu al hacer Scroll */
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        const idCurrentSection = entry.target.id;
+        const currentLink = document.querySelector(`.nav__list .${idCurrentSection}`);
+
+        if (entry.isIntersecting) {
+            document.querySelector('.actived__link').classList.remove('actived__link');
+            currentLink.classList.add('actived__link');
+            console.log(currentLink);
+        }
+    });
+}, {
+    rootMargin: '0px',
+    threshold: 0.8
+});
+
+const sections = document.querySelectorAll('.section');
+sections.forEach((section) => observer.observe(section));
---
+++ js/typed__script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#typing', {
+    strings: ['', 'Desarrollador Web', 'Ing. en Computacion', 'Autodidacta', 'Proactivo'],
+    typeSpeed: 110,
+    BackSpeed: 60,
+    loop: true
+});
